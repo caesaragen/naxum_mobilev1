6049a206784d3f5c97db6fbfb2259a07b0f03d44
Joel Aduma <caesaragen@gmail.com>
2023-03-10 11:28:54 +0300

    complete profile screen ui

--- screens/DrawerScreens/ProfileScreen.js
+++ screens/DrawerScreens/ProfileScreen.js
@@ -13,7 +13,7 @@ import {
   Stack,
   Text,
 } from "native-base";
-import { MaterialIcons } from "@expo/vector-icons";
+import { MaterialIcons, Ionicons } from "@expo/vector-icons";
 
 const ProfileScreen = () => {
   const [firstName, setFirstName] = useState("");
@@ -24,94 +24,112 @@ const ProfileScreen = () => {
 
   return (
     <View style={styles.container}>
-      <Box
-        bg="#307ecc"
-        width="100%"
-        height={180}
-        alignItems="center"
-        justifyContent="center"
-      >
-        <Avatar
-          source={{
-            uri:
-              "https://images.unsplash.com/photo-1608726138476-433c5e7e1661?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzN8fGNvbnRhY3QlMjBkZWZhdWx0JTIwaW1hZ2VzfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-          }}
-          size="2xl"
+      <Stack space={4} w="100%" maxW="300px" mx="auto" alignItems="center">
+        <Box
+          bg="#fff"
+          width="100%"
+          // height={180}
+          my={4}
+          alignItems="center"
+          justifyContent="center"
         >
-          <Badge
-            position="absolute"
-            size={8}
-            borderRadius={999}
-            bg="#70B5F9"
-            borderWidth={1}
-            borderColor="#fff"
-          />
-        </Avatar>
-        <Button
-          variant="outline"
-          mt={4}
-          size="sm"
-          onPress={() => console.log("Top Badges")}
-        >
-          Top Badges
-        </Button>
-      </Box>
-      <Stack direction="row" bg="#f5f5f5" p={4}>
-        <Button
-          variant={selectedTab === "profile" ? "solid" : "outline"}
-          onPress={() => setSelectedTab("profile")}
-        >
-          Profile
-        </Button>
-        <Button
-          variant={selectedTab === "social" ? "solid" : "outline"}
-          onPress={() => setSelectedTab("social")}
-        >
-          Social
-        </Button>
-        <Button
-          variant={selectedTab === "links" ? "solid" : "outline"}
-          onPress={() => setSelectedTab("links")}
-        >
-          Links
-        </Button>
+<Avatar
+  source={{
+    uri:
+    "https://as2.ftcdn.net/v2/jpg/00/65/77/27/1000_F_65772719_A1UV5kLi5nCEWI0BNLLiFaBPEkUbv5Fv.jpg",
+  }}
+  size="2xl"
+  style={styles.avatar}
+>
+  <Avatar.Badge style={styles.badge}>
+    <Icon
+      as={<Ionicons name="md-pencil" />}
+      color="#fff"
+      size={5}
+    />
+  </Avatar.Badge>
+</Avatar>
+          <Button
+            variant="solid"
+            mt={4}
+            size="md"
+            width={"99%"}
+            onPress={() => console.log("Top Badges")}
+          >
+            Top Badges
+          </Button>
+        </Box>
+        <Button.Group width={"100%"} isAttached mx={{
+          base: "auto",
+          md: 0
+        }} size="sm">
+          <Button
+            variant={selectedTab === "profile" ? "solid" : "outline"}
+            onPress={() => setSelectedTab("profile")}
+            width={"33%"}
+          >
+            Profile
+          </Button>
+          <Button
+            variant={selectedTab === "social" ? "solid" : "outline"}
+            onPress={() => setSelectedTab("social")}
+            width={"33%"}
+          >
+            Social
+          </Button>
+          <Button
+            variant={selectedTab === "links" ? "solid" : "outline"}
+            onPress={() => setSelectedTab("links")}
+            width={"33%"}
+          >
+            Links
+          </Button>
+        </Button.Group>
+
+        <Box p={4} width={"100%"}>
+          {selectedTab === "profile" && (
+            <Stack space={4} width={"100%"}>
+              <FormControl>
+                <Input
+                  variant="underlined"
+                  placeholder="First Name"
+                  isRequired
+                  value={firstName}
+                  onChangeText={(value) => setFirstName(value)}
+                />
+              </FormControl>
+              <FormControl>
+                <Input
+                  variant={"underlined"}
+                  placeholder="Last Name"
+                  isRequired
+                  value={lastName}
+                  onChangeText={(value) => setLastName(value)}
+                />
+              </FormControl>
+              <FormControl>
+                <Input
+                  variant={"underlined"}
+                  placeholder="Mobile"
+                  isRequired
+                  value={mobile}
+                  onChangeText={(value) => setMobile(value)}
+                  keyboardType="phone-pad"
+                />
+              </FormControl>
+              <FormControl>
+                <Input
+                  variant={"underlined"}
+                  placeholder="Email"
+                  value={email}
+                  onChangeText={(value) => setEmail(value)}
+                  keyboardType="email-address"
+                />
+              </FormControl>
+            </Stack>
+          )}
+        </Box>
       </Stack>
-      <Box p={4}>
-        {selectedTab === "profile" && (
-          <Stack space={4}>
-            <FormControl>
-              <FormControl.Label>First Name</FormControl.Label>
-              <Input
-                value={firstName}
-                onChangeText={(value) => setFirstName(value)}
-              />
-            </FormControl>
-            <FormControl>
-              <FormControl.Label>Last Name</FormControl.Label>
-              <Input
-                value={lastName}
-                onChangeText={(value) => setLastName(value)}
-              />
-            </FormControl>
-            <FormControl>
-              <FormControl.Label>Mobile</FormControl.Label>
-              <Input
-                value={mobile}
-                onChangeText={(value) => setMobile(value)}
-                keyboardType="phone-pad"
-              />
-            </FormControl>
-            <FormControl>
-              <FormControl.Label>Email</FormControl.Label>
-              <Input
-                value={email}
-                onChangeText={(value) => setEmail(value)}
-                keyboardType="email-address"
-              />
-            </FormControl>
-          </Stack>
-        )}
-      </Box>
     </View>
   );
 };
@@ -121,6 +139,30 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
   },
+  avatar: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  badge: {
+    backgroundColor: "grey",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+
 });
 
 
